23cee23bf471186c5bd36200ea9b66a2d18bb771
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-07-01 15:04:47 +0200

    Teaspoon javascript spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -34,7 +33,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 end
